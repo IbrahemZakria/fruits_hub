23ff2e7c66a8945b6a8cfe7cdb665582d6ad97cc
IbrahemZakria <https://github.com/IbrahemZakria>
2025-07-24 18:30:17 +0200

    adding fire base to project

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:fruit_hub/bloco_bserver.dart';
 import 'package:fruit_hub/core/utils/services/shared_preferance.dart';
 import 'package:fruit_hub/fruit_hub_app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   Bloc.observer = MyBlocObserver();
   await SharedPreferance.init();
   runApp(const FruitHubApp());
